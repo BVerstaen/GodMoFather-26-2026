34828b4a63c4a84a91ea746e4c758fec88ab67eb
Elise <elizerosecampestrini@gmail.com>
2026-09-09 15:09:07 +0200

    symboles et animation

--- Assets/_Project/Scripts/GhostInteraction/GhostInteractionInput.cs
+++ Assets/_Project/Scripts/GhostInteraction/GhostInteractionInput.cs
@@ -1,60 +1,37 @@
-using System;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 public class GhostInteractionInput : MonoBehaviour
 {
-    [Header("Input")]
-    [SerializeField] private InputActionReference _ghostInteraction_0;
-    [SerializeField] private InputActionReference _ghostInteraction_1;
-    [SerializeField] private InputActionReference _ghostInteraction_2;
-    [SerializeField] private InputActionReference _ghostInteraction_3;
-    [SerializeField] private InputActionReference _ghostInteraction_4;
-    [SerializeField] private InputActionReference _ghostInteraction_5;
-    [SerializeField] private InputActionReference _ghostInteraction_6;
-    [SerializeField] private InputActionReference _ghostInteraction_7;
-
-    [Header("UnityEvents")]
-    [SerializeField] private UnityEvent _onInteraction_0;
-    [SerializeField] private UnityEvent _onInteraction_1;
-    [SerializeField] private UnityEvent _onInteraction_2;
-    [SerializeField] private UnityEvent _onInteraction_3;
-    [SerializeField] private UnityEvent _onInteraction_4;
-    [SerializeField] private UnityEvent _onInteraction_5;
-    [SerializeField] private UnityEvent _onInteraction_6;
-    [SerializeField] private UnityEvent _onInteraction_7;
+    [SerializeField] private Dictionary<InputActionReference, Sprite> _ghostInteractions = new Dictionary<InputActionReference, Sprite>();
+    [SerializeField] private GameObject _symbolPrefab;
+    [SerializeField] private Transform _symbolParent;
 
     private void OnEnable()
     {
-        _ghostInteraction_0.action.started += TriggerInteraction0;
-        _ghostInteraction_1.action.started += TriggerInteraction1;
-        _ghostInteraction_2.action.started += TriggerInteraction2;
-        _ghostInteraction_3.action.started += TriggerInteraction3;
-        _ghostInteraction_3.action.started += TriggerInteraction4;
-        _ghostInteraction_3.action.started += TriggerInteraction5;
-        _ghostInteraction_3.action.started += TriggerInteraction6;
-        _ghostInteraction_3.action.started += TriggerInteraction7;
+        foreach (KeyValuePair<InputActionReference, Sprite> kvp in _ghostInteractions)
+        {
+            kvp.Key.action.performed += act => DisplayInteraction(kvp.Value);
+        }
     }
 
     private void OnDisable()
     {
-        _ghostInteraction_0.action.started -= TriggerInteraction0;
-        _ghostInteraction_1.action.started -= TriggerInteraction1;
-        _ghostInteraction_2.action.started -= TriggerInteraction2;
-        _ghostInteraction_3.action.started -= TriggerInteraction3;
-        _ghostInteraction_3.action.started -= TriggerInteraction4;
-        _ghostInteraction_3.action.started -= TriggerInteraction5;
-        _ghostInteraction_3.action.started -= TriggerInteraction6;
-        _ghostInteraction_3.action.started -= TriggerInteraction7;
+        foreach (KeyValuePair<InputActionReference, Sprite> kvp in _ghostInteractions)
+        {
+            kvp.Key.action.performed -= act => DisplayInteraction(kvp.Value);
+        }
     }
 
-    private void TriggerInteraction0(InputAction.CallbackContext context) => _onInteraction_0?.Invoke();
-    private void TriggerInteraction1(InputAction.CallbackContext context) => _onInteraction_1?.Invoke();
-    private void TriggerInteraction2(InputAction.CallbackContext context) => _onInteraction_2?.Invoke();
-    private void TriggerInteraction3(InputAction.CallbackContext context) => _onInteraction_3?.Invoke();
-    private void TriggerInteraction4(InputAction.CallbackContext context) => _onInteraction_4?.Invoke();
-    private void TriggerInteraction5(InputAction.CallbackContext context) => _onInteraction_5?.Invoke();
-    private void TriggerInteraction6(InputAction.CallbackContext context) => _onInteraction_6?.Invoke();
-    private void TriggerInteraction7(InputAction.CallbackContext context) => _onInteraction_7?.Invoke();
+    private void DisplayInteraction(Sprite symbol)
+    {
+        GameObject interactionGO = Instantiate(_symbolPrefab, _symbolParent);
+        interactionGO.transform.position = _symbolParent.position;
+        if (interactionGO.TryGetComponent<SymbolAnimation>(out SymbolAnimation symbolAnim))
+        {
+            symbolAnim.Init(symbol);
+        }
+    }
+
 }
---
+++ Assets/_Project/Scripts/GhostInteraction/SymbolAnimation.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SymbolAnimation : MonoBehaviour
+{
+    [SerializeField] private Image _symbolIMG;
+    [SerializeField] private Vector2 _XGap;
+    [SerializeField] private Vector2 _YGap;
+    [SerializeField] private float _animDuration;
+
+    public void Init(Sprite symbol)
+    {
+        _symbolIMG.sprite = symbol;
+
+        StartCoroutine(Animate());
+    }
+
+    private IEnumerator Animate()
+    {
+        float targetX = Random.Range(_XGap.x, _XGap.y);
+        float targetY = Random.Range(_YGap.x, _YGap.y);
+
+        Vector2 TargetPos = new Vector2(transform.position.x + targetX, transform.position.y + targetY);
+        Vector2 StartPos = transform.position;
+
+        float time = 0;
+
+        while (time < _animDuration)
+        {
+            time += Time.deltaTime;
+            float t = time / _animDuration;
+
+            transform.position = Vector2.Lerp(StartPos, TargetPos, t);
+
+            Color symbolColor = _symbolIMG.color;
+            symbolColor.a = Mathf.Lerp(0, 1, t);
+            _symbolIMG.color = symbolColor;
+
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(0.1f);
+
+        float timeToDisapear = 0;
+
+        while (timeToDisapear < 1.5f)
+        {
+            timeToDisapear += Time.deltaTime;
+            float t = time / _animDuration;
+
+            Color symbolColor = _symbolIMG.color;
+            symbolColor.a = Mathf.Lerp(1, 0, t);
+            _symbolIMG.color = symbolColor;
+
+            yield return null;
+        }
+
+
+        yield return new WaitForSeconds(0.3f);
+        Destroy(gameObject);
+    }
+
+}
