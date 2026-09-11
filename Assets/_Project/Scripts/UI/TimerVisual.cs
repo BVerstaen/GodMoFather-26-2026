9f42f6e3e0b19599c2bc3d65a36c677f5771d869
Elise <elizerosecampestrini@gmail.com>
2026-09-11 10:14:07 +0200

    timer bounce

--- Assets/_Project/Scripts/UI/TimerVisual.cs
+++ Assets/_Project/Scripts/UI/TimerVisual.cs
@@ -1,11 +1,22 @@
+using System.Collections;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TimerVisual : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _timerText;
-    [SerializeField] private int _lastSecondsWarning = 10;
+    [SerializeField] private int _lastSecondsWarning = 11; 
+    [SerializeField] private float _bouceDuration = 0.3f;
+    [SerializeField] private float _timeBtwBounce = 0.7f;
+    [SerializeField] private float _add = 15;
+    private float _startingSize;
     private bool _isInWarning;
+
+    private void Awake()
+    {
+        _startingSize = _timerText.fontSize;
+    }
 
     public void UpdateTimer(float _currentTimer)
     {
@@ -19,7 +30,26 @@ public class TimerVisual : MonoBehaviour
             _isInWarning = true;
 
             _timerText.color = Color.red;
+            StartCoroutine(Bounce());
         }
+    }
+
+    private IEnumerator Bounce()
+    {
+        while (true)
+        {
+            float time = 0f;
+            while (time < _bouceDuration)
+            {
+                time += Time.deltaTime;
+                float t = time / _bouceDuration;
+
+                _timerText.fontSize = _startingSize + Mathf.Sin(t * Mathf.PI) * _add;
+
+                yield return null;
+            }
+            yield return new WaitForSeconds(_timeBtwBounce);
+        } 
     }
 
 
